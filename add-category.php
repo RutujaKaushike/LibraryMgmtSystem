8c7755dfaecdaea1236cde845af33b13b17e28e6
Sankalp Bhandari <sankalp.bhandari@gmail.com>
2019-04-07 19:08:53 -0500

    Changed category title

--- add-category.php
+++ add-category.php
@@ -36,7 +36,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
     <meta name="description" content=""/>
     <meta name="author" content=""/>
-    <title>Online Library Management System | Add Author</title>
+    <title>Online Library Management System | Add Category</title>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.7.6/css/mdb.min.css" rel="stylesheet">
